f49f10e7bbf3572d1849fe3d5415e68a5fbf3f06
Joshua Karanja <jokaranja26@gmail.com>
2025-09-16 21:48:16 +0300

    stage files

--- server/app.py
+++ server/app.py
@@ -70,6 +70,7 @@ api.add_resource(Newsletters, '/newsletters')
 class NewsletterByID(Resource):
 
     def get(self, id):
+    
 
         response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
 
@@ -78,6 +79,36 @@ class NewsletterByID(Resource):
             200,
         )
 
+        return response
+    def patch (self ,id):
+
+        
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr , request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+        
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+    
+    def delete (self,id):
+        record = Newsletter.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message":"record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
